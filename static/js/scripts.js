84416aabf556e6505bc0bc03ab7bcdf1ffe1958b
zhangbofeng <zhangbofeng@qq.com>
2024-02-27 10:43:02 +0800

    移动端导航栏优化push

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,56 +19,6 @@ function scrollFunction() {
 	}
 }
 
-// Navbar on mobile
-let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
-
-for (let i = 0; i < elements.length; i++) {
-	elements[i].addEventListener("click", () => {
-		document.querySelector(".offcanvas-collapse").classList.toggle("open");
-	});
-}
-
-// document.querySelector(".navbar-toggler").addEventListener("click", () => {
-//   	document.querySelector(".offcanvas-collapse").classList.toggle("open");
-// });
-
-// Hover on desktop
-function toggleDropdown(e) {
-	const _d = e.target.closest(".dropdown");
-	let _m = document.querySelector(".dropdown-menu", _d);
-
-	setTimeout(
-		function () {
-		const shouldOpen = _d.matches(":hover");
-		_m.classList.toggle("show", shouldOpen);
-		_d.classList.toggle("show", shouldOpen);
-
-		_d.setAttribute("aria-expanded", shouldOpen);
-		},
-		e.type === "mouseleave" ? 300 : 0
-	);
-}
-
-// On hover
-const dropdownCheck = document.querySelector('.dropdown');
-
-if (dropdownCheck !== null) { 
-	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
-	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
-
-	// On click
-	document.querySelector(".dropdown").addEventListener("click", (e) => {
-		const _d = e.target.closest(".dropdown");
-		let _m = document.querySelector(".dropdown-menu", _d);
-		if (_d.classList.contains("show")) {
-			_m.classList.remove("show");
-			_d.classList.remove("show");
-		} else {
-			_m.classList.add("show");
-			_d.classList.add("show");
-		}
-	});
-}
 
 
 /* Rotating Text - Word Cycle */
@@ -115,6 +65,58 @@ var cardSlider = new Swiper('.card-slider', {
 	}
 });
 
+// Navbar on mobile
+let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", () => {
+        document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+}
+
+let selectors = document.querySelector(".navbar-toggler");
+if(selectors !== null){
+    selectors.addEventListener("click", () => {
+        document.querySelector(".offcanvas-collapse").classList.toggle("open");
+    });
+}
+
+// Hover on desktop
+function toggleDropdown(e) {
+    const _d = e.target.closest(".dropdown");
+    let _m = document.querySelector(".dropdown-menu", _d);
+
+    setTimeout(
+        function () {
+            const shouldOpen = _d.matches(":hover");
+            _m.classList.toggle("show", shouldOpen);
+            _d.classList.toggle("show", shouldOpen);
+
+            _d.setAttribute("aria-expanded", shouldOpen);
+        },
+        e.type === "mouseleave" ? 300 : 0
+    );
+}
+
+// On hover
+const dropdownCheck = document.querySelector('.dropdown');
+
+if (dropdownCheck !== null) {
+    document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+    document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+
+    // On click
+    document.querySelector(".dropdown").addEventListener("click", (e) => {
+        const _d = e.target.closest(".dropdown");
+        let _m = document.querySelector(".dropdown-menu", _d);
+        if (_d.classList.contains("show")) {
+            _m.classList.remove("show");
+            _d.classList.remove("show");
+        } else {
+            _m.classList.add("show");
+            _d.classList.add("show");
+        }
+    });
+}
 
 /* Back To Top Button */
 // Get the button
