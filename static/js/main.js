3de4ade80c65f39205ffa52b52edb71615c4f3bc
zbf <zbf@uija.cn>
2024-04-01 01:17:43 +0800

    官网优化

--- static/js/main.js
+++ static/js/main.js
@@ -1,67 +1,5 @@
-
-// 监听页面滚动事件
-window.addEventListener("scroll", function() {
-
-    // 获取导航栏元素
-
-    var navbar = document.querySelector(".navbar");
-
-    var twoBoxs = document.getElementsByClassName('two_box');
-
-    // 计算页面滚动距离
-
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-
-    // 计算滚动距离与导航栏高度之比
-
-    var ratio = scrollTop / navbar.offsetHeight;
-
-    // 根据比值设置导航栏的透明度
-    if (ratio < 0.5) {
-
-        // 移动端样式
-        if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-            let i = document.querySelector("#head_menu")
-            if(i.classList.contains('n-hamburger-menu-bars')){
-                navbar.style.removeProperty("background-color");
-                navbar.style.removeProperty("height");
-            }else{
-                navbar.style.backgroundColor = "#00000070";
-                navbar.style.height = " 7%";
-            }
-        }else{
-            // 取消滚动回到置顶导航栏透明样式
-            navbar.style.backgroundColor = "rgba(255, 255, 255, " + ratio + ")";
-        }
-
-
-        // 检查是否找到了元素
-        if (twoBoxs.length > 0) {
-            // 遍历所有匹配的元素并设置样式
-            for (var i = 0; i < twoBoxs.length; i++) {
-                twoBoxs[i].style.removeProperty("background-color");
-                twoBoxs[i].style.removeProperty("margin-top");
-            }
-        }
-    } else {
-
-        // navbar.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-        navbar.style.background = "rgba(0,0,0,.7)";
-        // 检查是否找到了元素
-        if (twoBoxs.length > 0) {
-            // 遍历所有匹配的元素并设置样式
-            for (var i = 0; i < twoBoxs.length; i++) {
-                twoBoxs[i].style.backgroundColor = "#333";
-                twoBoxs[i].style.marginTop = "5px";
-            }
-        }
-
-    }
-
-});
-
-var isMobile = false;
-
+// //scrollcue js
+scrollCue.init();
 $(function(){
 
     //======< scrollcue js >======
@@ -72,8 +10,102 @@ $(function(){
         smartSpeed: 5000
 
     })
+	
+	window.onload = function() {
+		// 移动端样式
+		if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+			var mheader = document.querySelector(".mheader");
+			var nav = mheader.querySelector("nav");
+			nav.classList.toggle("navbar");
+		} else {
+			var wrap = document.querySelector(".wrap");
+			var nav = wrap.querySelector("nav");
+			nav.classList.toggle("navbar");
+		}
+		// scrollFunction();
+	};
+	
+	/* Navigation*/
+	// 监听页面滚动事件
+	// Collapse the navbar by adding the top-nav-collapse class
+	// window.onscroll = function() {
+	// 	scrollFunction();
+	// };
+	
+	// // 页面滚动，导航栏锁定置顶样式
+	// function scrollFunction() {
+	// 	if (document.documentElement.scrollTop > 30) {
+	// 		document.querySelector(".navbar").classList.add("top-nav-collapse");
+	// 	} else if (document.documentElement.scrollTop < 30) {
+	// 		document.querySelector(".navbar").classList.remove("top-nav-collapse");
+	// 	}
+	// }
+	
+	
+	// 监听页面滚动事件
+	window.addEventListener("scroll", function() {
+	
+	    // 获取导航栏元素
+	
+	    var navbar = document.querySelector(".navbar");
+	
+	    var twoBoxs = document.getElementsByClassName('two_box');
+	
+	    // 计算页面滚动距离
+	
+	    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+	
+	    // 计算滚动距离与导航栏高度之比
+	
+	    var ratio = scrollTop / navbar.offsetHeight;
+	
+	    // 根据比值设置导航栏的透明度
+	    if (ratio < 0.5) {
+	
+	        // 移动端样式
+	        if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+	            let i = document.querySelector("#navbar")
+	            if(i.classList.contains('navbar')){
+	                navbar.style.removeProperty("background-color");
+	                navbar.style.removeProperty("height");
+	            }else{
+	                navbar.style.backgroundColor = "#00000070";
+	                navbar.style.height = " 7%";
+	            }
+	        }else{
+	            // 取消滚动回到置顶导航栏透明样式
+	            navbar.style.backgroundColor = "rgba(255, 255, 255, " + ratio + ")";
+				
+				document.querySelector(".navbar").classList.toggle("top-nav-collapse");
+	        }
+	
+	
+	        // 检查是否找到了元素
+	        if (twoBoxs.length > 0) {
+	            // 遍历所有匹配的元素并设置样式
+	            for (var i = 0; i < twoBoxs.length; i++) {
+	                twoBoxs[i].style.removeProperty("background-color");
+	            }
+	        }
+			
+			
+	    } else {
+	
+	        // navbar.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+	        navbar.style.background = "rgba(0,0,0,.7)";
+	        // 检查是否找到了元素
+	        if (twoBoxs.length > 0) {
+	            // 遍历所有匹配的元素并设置样式
+	            for (var i = 0; i < twoBoxs.length; i++) {
+	                twoBoxs[i].style.backgroundColor = "#333";
+	            }
+	        }
+			
+			document.querySelector(".navbar").classList.remove("top-nav-collapse");
+	
+	    }
+	
+	});
 });
-// //scrollcue js
-scrollCue.init();
 
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,189 +2,104 @@
 
 /* Navigation*/
 // Collapse the navbar by adding the top-nav-collapse class
-window.onscroll = function () {
-	scrollFunction();
+window.onscroll = function() {
+	// scrollFunction();
 	// scrollFunctionBTT(); // back to top button
-};
-
-window.onload = function () {
-    // 移动端样式
-    if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        var mheader = document.querySelector(".mheader");
-        var nav = mheader.querySelector("nav");
-        nav.classList.toggle("navbar");
-    }else{
-        var wrap = document.querySelector(".wrap");
-        var nav = wrap.querySelector("nav");
-        nav.classList.toggle("navbar");
-    }
-    scrollFunction();
 };
 
 // 在用户刷新页面之前将页面滚动到顶部
 window.addEventListener('beforeunload', function() {
-    window.scrollTo(0, 0);
+	window.scrollTo(0, 0);
 });
-
-function scrollFunction() {
-    if (document.documentElement.scrollTop > 30) {
-        document.querySelector(".navbar").classList.add("top-nav-collapse");
-    } else if ( document.documentElement.scrollTop < 30 ) {
-        document.querySelector(".navbar").classList.remove("top-nav-collapse");
-    }
-}
-
 
 
 /* Rotating Text - Word Cycle */
 var checkReplace = document.querySelector('.replace-me');
-if (checkReplace !== null) { 
+if (checkReplace !== null) {
 	var replace = new ReplaceMe(document.querySelector('.replace-me'), {
-		animation: 'animated fadeIn',                       // Animation class or classes
-		speed: 2000,                                        // Delay between each phrase in miliseconds
-		separator: ',',                                     // Phrases separator
-		hoverStop: false,                                   // Stop rotator on phrase hover
-		clickChange: false,                                 // Change phrase on click
-		loopCount: 'infinite',                              // Loop Count - 'infinite' or number
-		autoRun: true,                                      // Run rotator automatically
-		onInit: false,                                      // Function
-		onChange: false,                                    // Function
-		onComplete: false                                   // Function
+		animation: 'animated fadeIn', // Animation class or classes
+		speed: 2000, // Delay between each phrase in miliseconds
+		separator: ',', // Phrases separator
+		hoverStop: false, // Stop rotator on phrase hover
+		clickChange: false, // Change phrase on click
+		loopCount: 'infinite', // Loop Count - 'infinite' or number
+		autoRun: true, // Run rotator automatically
+		onInit: false, // Function
+		onChange: false, // Function
+		onComplete: false // Function
 	});
 }
 
 
-//banner
-let swiperBanner =  document.querySelector(".swiper");
 
-if(swiperBanner!=null){
-    var swiper = new Swiper('.swiper-container-pro', {
-        slidesPerView: 4,
-        slidesPerColumn: 1,
-        spaceBetween: 20,
-        autoplay:4000,
-        preventClicks : false,
-        nextButton: '.swiper-button-next-pro',
-        prevButton: '.swiper-button-prev-pro',
-        paginationClickable: true,
-        breakpoints: {
-            1199: {
-                slidesPerView: 4,
-                spaceBetween: 16,
-            },
-            991: {
-                slidesPerView: 3,
-                spaceBetween: 12,
-            },
-            767: {
-                slidesPerView: 2,
-                spaceBetween: 18,
-            },
-            567: {
-                slidesPerView: 2,
-                spaceBetween: 12,
-            },
-            372: {
-                slidesPerView: 2,
-                spaceBetween: 10,
-            }
-        }
-    });
-
-    /* Card Slider - Swiper */
-    var cardSlider = new Swiper('.card-slider', {
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-        },
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        slidesPerView: 3,
-        spaceBetween: 70,
-        breakpoints: {
-            // when window is <= 767px
-            767: {
-                slidesPerView: 1
-            },
-            // when window is <= 991px
-            991: {
-                slidesPerView: 2,
-                spaceBetween: 40
-            }
-        }
-    });
-}
 
 // Navbar on mobile
 let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
 for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", () => {
-        document.querySelector(".offcanvas-collapse").classList.toggle("open");
-        document.querySelector(".n-hamburger-menu-bars").classList.toggle("open");
-    });
+	elements[i].addEventListener("click", () => {
+		document.querySelector(".offcanvas-collapse").classList.toggle("open");
+		document.querySelector(".n-hamburger-menu-bars").classList.toggle("open");
+	});
 }
 
 let selectors = document.querySelector(".navbar-toggler");
-if(selectors !== null){
-    selectors.addEventListener("click", () => {
-        document.querySelector(".offcanvas-collapse").classList.toggle("open");
-        // 切换头部菜单按钮图标
-        let i = document.querySelector("#head_menu")
-        if (i !== null ) {
-            i.classList.toggle("n-header-crossing-icon");
-            i.classList.toggle("n-hamburger-menu-bars");
-        }
+if (selectors !== null) {
+	selectors.addEventListener("click", () => {
+		document.querySelector(".offcanvas-collapse").classList.toggle("open");
+		// 切换头部菜单按钮图标
+		let i = document.querySelector("#head_menu")
+		if (i !== null) {
+			i.classList.toggle("nav_button");
+			i.classList.toggle("n-header-crossing-icon");
+		}
 
-        document.querySelector(".navbar").classList.toggle('main-navigation');
+		document.querySelector(".navbar").classList.toggle('main-navigation');
 
 
-        var navbar = document.querySelector(".navbar");
-        if(!navbar.classList.contains('top-nav-collapse')){
-            navbar.style.removeProperty("background-color");
-            navbar.style.removeProperty("height");
-        }
-    });
+		var navbar = document.querySelector(".navbar");
+		if (!navbar.classList.contains('top-nav-collapse')) {
+			navbar.style.removeProperty("background-color");
+			navbar.style.removeProperty("height");
+		}
+	});
 }
 
 // Hover on desktop
 function toggleDropdown(e) {
-    const _d = e.target.closest(".dropdown");
-    let _m = document.querySelector(".dropdown-menu", _d);
+	const _d = e.target.closest(".dropdown");
+	let _m = document.querySelector(".dropdown-menu", _d);
 
-    setTimeout(
-        function () {
-            const shouldOpen = _d.matches(":hover");
-            _m.classList.toggle("show", shouldOpen);
-            _d.classList.toggle("show", shouldOpen);
+	setTimeout(
+		function() {
+			const shouldOpen = _d.matches(":hover");
+			_m.classList.toggle("show", shouldOpen);
+			_d.classList.toggle("show", shouldOpen);
 
-            _d.setAttribute("aria-expanded", shouldOpen);
-        },
-        e.type === "mouseleave" ? 300 : 0
-    );
+			_d.setAttribute("aria-expanded", shouldOpen);
+		},
+		e.type === "mouseleave" ? 300 : 0
+	);
 }
 
 // On hover
 const dropdownCheck = document.querySelector('.dropdown');
 
 if (dropdownCheck !== null) {
-    document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
-    document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
 
-    // On click
-    document.querySelector(".dropdown").addEventListener("click", (e) => {
-        const _d = e.target.closest(".dropdown");
-        let _m = document.querySelector(".dropdown-menu", _d);
-        if (_d.classList.contains("show")) {
-            _m.classList.remove("show");
-            _d.classList.remove("show");
-        } else {
-            _m.classList.add("show");
-            _d.classList.add("show");
-        }
-    });
+	// On click
+	document.querySelector(".dropdown").addEventListener("click", (e) => {
+		const _d = e.target.closest(".dropdown");
+		let _m = document.querySelector(".dropdown-menu", _d);
+		if (_d.classList.contains("show")) {
+			_m.classList.remove("show");
+			_d.classList.remove("show");
+		} else {
+			_m.classList.add("show");
+			_d.classList.add("show");
+		}
+	});
 }
 
 /* Back To Top Button */
